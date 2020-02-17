c8ccc634114a866935f5998785014996625278c2
jburkhardt <johannes.burkhardt@gmail.com>
2020-02-17 10:42:24 +0100

    adds ex3

---
+++ ex/ex3.py
@@ -0,0 +1,18 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Rosters", 100 - 25 * 3 % 4
+
+print "modulus operator: 25 * 3 % 4"
+print 25 * 3 % 4
+
+print "modulus operator: 4 % 3"
+print 4 % 3
+
+print "Now I will count my eggs:"
+
+print 3 + 2 + 1 - 5 +4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7

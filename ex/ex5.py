183b2d85e386f62810bf2d8c17ac8cc8c001e059
jburkhardt <johannes.burkhardt@gmail.com>
2020-02-18 09:18:27 +0100

    adds ex5

---
+++ ex/ex5.py
@@ -0,0 +1,17 @@
+# -- coding: utf-8 --
+my_name = 'Jay'
+my_age = 15
+my_height = 190
+my_weight = 80
+my_eyes = 'green'
+my_teeth = 'white'
+my_hair = 'blonde'
+
+print "Let´s talk about %s." % my_name
+print "He´s %d cm tall." % my_height
+print "He´s %d kg heavy." % my_weight
+print "Actually that´s not too heavy."
+print "He´s got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d adn %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
